eef1dd2adf6625a535b37079f960177362794c17
Ryo Matsukawa <matsukawa.ryo42@gmail.com>
2023-05-29 14:20:24 +0900

    add: 棒グラフで表示する

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,106 @@
 import React, { useState } from 'react';
 import cronParser from 'cron-parser';
 import moment from 'moment-timezone';
-import TimeWheel from './TimeWheel';
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+} from 'recharts';
 
-function App() {
-  const [cronExpression, setCronExpression] = useState<string>('*/3 0-8 * * 6');
-  const [timeZone, setTimeZone] = useState<string>('UTC');
-  const [parsedDates, setParsedDates] = useState<string[]>([]);
+type BarData = {
+  hour: number;
+  day: number;
+  dayLabel: string;
+  cronApplied: boolean;
+  time: string;
+};
 
-  const handleCronExpressionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+const DAYS = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+const App = () => {
+  const [cronExpression, setCronExpression] = useState<string>('* * * * *');
+  const [parsedData, setParsedData] = useState<BarData[]>([]);
+
+  const handleCronExpressionChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
     setCronExpression(e.target.value);
-  }
-
-  const handleTimeZoneChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setTimeZone(e.target.value);
-  }
+  };
 
   const visualizeCron = () => {
-    let options = {
-      currentDate: new Date(),
-      tz: timeZone,
-    };
-    let interval = cronParser.parseExpression(cronExpression, options);
-    let dates: string[] = [];
-    for (let i = 0; i < 24; i++) {
-      const date = interval.next().toDate();
-      const utcHour = moment(date).utc().hour();
-      const jstHour = moment(date).tz('Asia/Tokyo').hour();
-
-      // UTC時間が0-8 (つまりJST時間が9-17)の間であれば、日付を追加
-      if (utcHour >= 0 && utcHour < 9) {
-        dates.push(moment(date).tz(timeZone).format('HH:mm') + " (JST: " + jstHour + ":00)");
+    const data: BarData[] = [];
+    for (let day = 0; day < 7; day++) {
+      for (let hour = 0; hour < 24; hour++) {
+        const date = moment().day(day).hour(hour);
+        const utcDate = date.clone().utc();
+        const cronSchedule = cronParser.parseExpression(cronExpression, {
+          currentDate: utcDate.toDate(),
+          tz: 'Asia/Tokyo',
+        });
+        const nextSchedule = moment(cronSchedule.next().toDate());
+        const cronApplied = nextSchedule.isSame(date, 'hour');
+        data.push({
+          day,
+          dayLabel: DAYS[day],
+          hour,
+          cronApplied,
+          time: `UTC: ${utcDate.format('YYYY-MM-DD HH:mm')} JST: ${date.format(
+            'YYYY-MM-DD HH:mm',
+          )}`,
+        });
       }
     }
-    setParsedDates(dates);
-  }
+    setParsedData(data);
+  };
+
+  const hours = Array.from({ length: 24 }, (_, i) => i);
 
   return (
     <div>
-      <input value={cronExpression} onChange={handleCronExpressionChange} placeholder="Enter cron expression" />
-      <select value={timeZone} onChange={handleTimeZoneChange}>
-        <option value="UTC">UTC</option>
-        <option value="Asia/Tokyo">JST</option>
-      </select>
+      <input
+        value={cronExpression}
+        onChange={handleCronExpressionChange}
+        placeholder="Enter cron expression (in JST)"
+      />
       <button onClick={visualizeCron}>Visualize</button>
-      <TimeWheel dates={parsedDates} />
+      <BarChart
+        width={1200}
+        height={500}
+        data={parsedData.filter((d) => d.cronApplied)}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="hour" type="number" ticks={hours} domain={[0, 23]} />
+        <YAxis dataKey="dayLabel" type="category" />
+        <Tooltip />
+        <Bar dataKey="cronApplied" fill="#8884d8">
+          {parsedData.map((entry, index) => (
+            <Cell
+              key={`cell-${index}`}
+              fill={entry.cronApplied ? '#8884d8' : '#ffffff00'}
+            />
+          ))}
+        </Bar>
+      </BarChart>
     </div>
   );
-}
+};
 
 export default App;
