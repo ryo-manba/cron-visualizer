a8d06158974e54ee22f9000c2a34eb0216b5c2e8
Ryo Matsukawa <matsukawa.ryo42@gmail.com>
2023-05-31 22:24:34 +0900

    add: BarからScatterに変更

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import React, { useState } from 'react';
 import cronParser from 'cron-parser';
 import moment from 'moment-timezone';
 import {
-  BarChart,
-  Bar,
-  Cell,
   XAxis,
   YAxis,
   Tooltip,
   CartesianGrid,
+  ScatterChart,
+  Scatter,
 } from 'recharts';
 
 type BarData = {
+  minute: number;
   hour: number;
   day: number;
   dayLabel: string;
@@ -40,65 +40,89 @@ const App = () => {
   };
 
   const visualizeCron = () => {
+    console.log('Start calculation');
     const data: BarData[] = [];
-    for (let day = 0; day < 7; day++) {
-      for (let hour = 0; hour < 24; hour++) {
-        const date = moment().day(day).hour(hour);
-        const utcDate = date.clone().utc();
-        const cronSchedule = cronParser.parseExpression(cronExpression, {
-          currentDate: utcDate.toDate(),
-          tz: 'Asia/Tokyo',
-        });
-        const nextSchedule = moment(cronSchedule.next().toDate());
-        const cronApplied = nextSchedule.isSame(date, 'hour');
-        data.push({
-          day,
-          dayLabel: DAYS[day],
-          hour,
-          cronApplied,
-          time: `UTC: ${utcDate.format('YYYY-MM-DD HH:mm')} JST: ${date.format(
-            'YYYY-MM-DD HH:mm',
-          )}`,
-        });
+
+    const options = {
+      currentDate: moment().startOf('week').toDate(),
+      endDate: moment().endOf('week').toDate(),
+      iterator: true,
+      tz: 'Asia/Tokyo',
+    };
+
+    try {
+      const interval = cronParser.parseExpression(cronExpression, options);
+
+      // eslint-disable-next-line no-constant-condition
+      while (true) {
+        try {
+          const obj = interval.next();
+          if ('done' in obj) {
+            const date = moment(obj.value.toString());
+            const day = date.day();
+            const hour = date.hour();
+            const minute = date.minute();
+            data.push({
+              minute,
+              day,
+              dayLabel: DAYS[day],
+              hour,
+              cronApplied: true,
+              time: `JST: ${date.format('YYYY-MM-DD HH:mm')}`,
+            });
+          }
+        } catch (e) {
+          break;
+        }
+      }
+    } catch (err) {
+      if (err instanceof Error) {
+        console.log('Error: ' + err.message);
       }
     }
+
+    // DEBUG:
+    data.map((entry, index) => {
+      if (entry.cronApplied) {
+        console.log('entry: ', entry);
+      }
+    });
+
     setParsedData(data);
+    console.log('Finish calculation: ', data);
   };
 
   const hours = Array.from({ length: 24 }, (_, i) => i);
 
   return (
     <div>
-      <input
-        value={cronExpression}
-        onChange={handleCronExpressionChange}
-        placeholder="Enter cron expression (in JST)"
-      />
-      <button onClick={visualizeCron}>Visualize</button>
-      <BarChart
+      <div>
+        <input
+          value={cronExpression}
+          onChange={handleCronExpressionChange}
+          placeholder="Enter cron expression (in JST)"
+          aria-label="Enter cron expression (in JST)"
+        />
+        <button onClick={visualizeCron}>Visualize</button>
+      </div>
+      <CartesianGrid strokeDasharray="10 10" />
+      <XAxis dataKey="hour" type="number" ticks={hours} domain={[0, 23]} />
+      <YAxis dataKey="dayLabel" type="category" />
+      <Tooltip />
+      <ScatterChart
         width={1200}
         height={500}
-        data={parsedData.filter((d) => d.cronApplied)}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
       >
-        <CartesianGrid strokeDasharray="3 3" />
+        <CartesianGrid strokeDasharray="10 10" />
         <XAxis dataKey="hour" type="number" ticks={hours} domain={[0, 23]} />
         <YAxis dataKey="dayLabel" type="category" />
-        <Tooltip />
-        <Bar dataKey="cronApplied" fill="#8884d8">
-          {parsedData.map((entry, index) => (
-            <Cell
-              key={`cell-${index}`}
-              fill={entry.cronApplied ? '#8884d8' : '#ffffff00'}
-            />
-          ))}
-        </Bar>
-      </BarChart>
+        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+        <Scatter
+          data={parsedData.filter((entry) => entry.cronApplied)}
+          fill="red"
+        />
+      </ScatterChart>
     </div>
   );
 };
