f97d4bb47f35ccb7cea286cbba61ccc394903366
Ryo Matsukawa <76232929+ryo-manba@users.noreply.github.com>
2025-07-21 18:45:45 +0900

    chore: update copyright year to 2023 - 2025

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,7 +14,7 @@ export const Footer = () => {
         >
           <FaGithub size={28} />
         </a>
-        <p className="text-sm text-gray-700">&copy; 2023 ryo-manba</p>
+        <p className="text-sm text-gray-700">&copy; 2023 - 2025 ryo-manba</p>
       </div>
     </footer>
   );
