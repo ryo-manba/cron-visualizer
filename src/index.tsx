a9e00df62df5a8a6a74d7b84f4ec8241a8cf8fd8
Ryo Matsukawa <76232929+ryo-manba@users.noreply.github.com>
2025-08-26 23:37:50 +0900

    add vercel analytics setting

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 );
 
