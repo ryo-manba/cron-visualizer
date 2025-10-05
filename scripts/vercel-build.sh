857b628acd24da045f8bd6b8615198576bb5f3a0
Ryo Matsukawa <76232929+ryo-manba@users.noreply.github.com>
2025-10-05 14:58:45 +0900

    add build settings

---
+++ scripts/vercel-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+
+# Install wasm-pack
+cargo install wasm-pack
+
+# Build the project
+pnpm build
